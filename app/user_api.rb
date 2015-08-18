410953e378bef26cf69c2ed471b10dde91b963ba
Betsy Lorton <elorton@gmail.com>
2015-08-18 14:45:20 -0500

    get /user/:id returns user info

--- app/user_api.rb
+++ app/user_api.rb
@@ -1,6 +1,15 @@
 class UserApi < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
+  get '/user/:id' do
+    user = User.find_by_id(params[:id])
+    if user
+      json({username: user.username, info: user.info})
+    else
+      json({message: 'invalid user id'})
+    end
+  end
+
   get '/' do
     json 'Hello World'
   end
--- spec/user_api_spec.rb
+++ spec/user_api_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe UserApi do
       end
     end
   end
+
+  describe 'get /user' do
+    context 'given id of an existing user' do
+      let!(:user) { User.create(username: 'betsy', password: 'password', info: 'stuff') }
+      it "returns the requested user's information" do
+        get "/user/#{user.id}"
+        expect(JSON.parse(last_response.body)['username']).to match 'betsy'
+        expect(JSON.parse(last_response.body)['info']).to match 'stuff'
+      end
+    end
+    context 'given an invalid user id' do
+      it "returns a message" do
+        get "/user/0"
+        expect(JSON.parse(last_response.body)['message']).to match 'invalid'
+      end
+    end
+  end
 end
